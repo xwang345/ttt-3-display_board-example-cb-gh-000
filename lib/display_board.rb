74b33588b84c445d6194d8d3cec3f9fcaec09009
Xiaochen Wang <ryan.wang23@hotmail.com>
2017-08-23 04:51:50 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,1 +1,9 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
+
+def display_board
+  puts "   |   |   "
+  2.times{puts "-----------"
+  puts "   |   |   "}
+end
+
+puts display_board
